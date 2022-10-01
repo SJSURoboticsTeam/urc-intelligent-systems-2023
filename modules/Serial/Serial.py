50eccd436ad9e3e540c5fe244ab0abaf12639c2e
acarrou <dridriswim@gmail.com>
2022-09-30 18:54:32 -0700

    adds serial class and demo to read and write with sjtwo

---
+++ modules/Serial/Serial.py
@@ -0,0 +1,36 @@
+import serial
+import time
+
+class SerialSystem:
+    def __init__(self, port, baudrate):
+        
+        self.port = port
+        self.baudrate = baudrate
+        self.ser = serial.Serial(
+                            port = port,
+                            baudrate = baudrate,
+                            parity = serial.PARITY_NONE,
+                            stopbits = serial.STOPBITS_ONE,
+                            bytesize = serial.EIGHTBITS,
+                            )
+        self.ser.rts = False
+        self.ser.dtr = False
+        self.ser.timeout = 0
+
+    def write_serial(self, message):
+        self.ser.write(message.encode("utf-8"))
+
+    def read_serial(self):
+        c = self.ser.read()
+        if len(c) == 0:
+            print("No data received")
+        else:
+            try:
+                c += self.ser.read(self.ser.inWaiting())
+                print(c.decode("utf-8"))
+            except:
+                print("Error decoding data")
+        time.sleep(1)
+
+    def close_serial(self):
+        self.ser.close()
