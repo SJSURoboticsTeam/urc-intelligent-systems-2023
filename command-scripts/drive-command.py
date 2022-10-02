a65966435ac09821126692ba0dc269f578e1fe9e
acarrou <dridriswim@gmail.com>
2022-10-02 05:52:45 +0100

    serial parse refactor for wheel homing

--- command-scripts/drive-command.py
+++ command-scripts/drive-command.py
@@ -9,6 +9,7 @@ import time
 port = "/dev/ttyUSB0"
 #example_json = '{"heartbeat_count":0,"is_operational":2,"wheel_orientation":0,"drive_mode":"D","speed":0,"angle":0}'
 
+
 get_initial_commands_url = "http://192.168.50.243:5000/drive"
 
 web_response = requests.get(get_initial_commands_url)
@@ -23,11 +24,16 @@ except:
     port = ports[0].device
     serial = SerialSystem(port, 38400)
 
+end = "Starting control loop..."
+
 while True:
-    serial.write_serial(web_response.text)
     response = serial.read_serial()
-    json_format = json.loads(response)
-    #json_format = json.loads(example_json)
-    requests.get(get_initial_commands_url, params=json_format)
-    #print("Data: " + web_response.text)
-    #time.sleep(1)
+    if response == "Starting control loop..." or end in response:
+        print("Parsing Data from Pi...")
+        while True:
+            serial.write_serial(web_response.text)
+            response = serial.read_serial()
+            json_format = json.loads(response)
+            #json_format = json.loads(example_json)
+            requests.get(get_initial_commands_url, params=json_format)
+            #print("Data: " + web_response.text)
