3c1d74dcd2764397d54439ccc7a4ed16e3db86d4
acarrou <dridriswim@gmail.com>
2023-01-07 15:39:35 -0800

    fixes serial communication for arduino and sj2

--- CommandScripts/drive-command.py
+++ CommandScripts/drive-command.py
@@ -1,13 +1,13 @@
-import sys, os
+import sys
 import requests
 import serial.tools.list_ports as port_list
-sys.path.insert(0, os.path.abspath(".."))
-from modules.Serial import SerialSystem
+sys.path.append( '../modules/Serial')
+from Serial import SerialSystem
 import json
 import time
 
 port = "/dev/ttyUSB0"
-get_initial_commands_url = "http://192.168.50.243:5000/drive"
+get_initial_commands_url = " http://172.20.10.3:5000/drive"
 
 web_response = requests.get(get_initial_commands_url)
 print("Getting data from: " + web_response.text)
@@ -23,13 +23,16 @@ except:
 
 homing_end = "Starting control loop..."
 while True:
-    response = serial.read_serial()
-    if homing_end in response:
-        while True:
-            web_response = requests.get(get_initial_commands_url)
-            response += serial.read_serial()
-            if response != "No data received":
-                serial.write_serial(web_response.text)
-            else:
-                continue
+    web_response = requests.get(get_initial_commands_url)
+    serial.read_serial(web_response.text)
+    # serial.write_serial(web_response.text)
+
+    # if homing_end in response:
+    #     while True:
+    #         web_response = requests.get(get_initial_commands_url)
+    #         response += serial.read_serial()
+    #         if response != "No data received":
+    #             serial.write_serial(web_response.text)
+    #         else:
+    #             continue
             
