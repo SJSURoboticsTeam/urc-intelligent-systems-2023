a37d6c769ccffd6d173095f68a62fbebe0fb1bce
Adrien Carrou <dridriswim@gmail.com>
2023-03-31 22:29:52 -0700

    :hammer: adds multiple weightpoints

--- demos/GridMap-Trajectory-demo/run.py
+++ demos/GridMap-Trajectory-demo/run.py
@@ -5,10 +5,11 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.colors import ListedColormap
 from queue import PriorityQueue
 from matplotlib.patches import Arrow
+import random
 
 
 class GridMapSimulator:
-    def __init__(self, resolution, map_width, map_height, target_x, target_y, num_initial_obstacles=20, interval=200):
+    def __init__(self, resolution, map_width, map_height, targets, num_initial_obstacles=20, interval=200):
         self.resolution = resolution
         self.map_width = map_width
         self.map_height = map_height
@@ -18,8 +19,9 @@ class GridMapSimulator:
         self.ani = None
         self.path_plot = None  # Add an attribute to store the path plot
         self.map = np.zeros((self.map_height, self.map_width))  # use a numpy array to represent the map
-        self.target_x = target_x
-        self.target_y = target_y
+        self.targets = targets
+        self.current_target_index = 0
+        self.target_x, self.target_y = targets[self.current_target_index]
         self.obstacles = self.generate_initial_obstacles(num_initial_obstacles)
         self.reached_destination = False
         self.rover_direction = 0
@@ -35,26 +37,29 @@ class GridMapSimulator:
         return obstacles
 
 
-    def init_visualization(self, target_x, target_y):
+    def init_visualization(self):
         self.fig, self.ax = plt.subplots()
         self.ax.set_title("Grid Map")
-        self.path_line, = self.ax.plot([], [], color='green')
-        self.path_plot, = self.ax.plot([], [], color='red', linestyle='-')  # Create a line plot for the path
+        self.path_line, = self.ax.plot([], [], color='lime')
+        self.path_plot, = self.ax.plot([], [], color='red', linestyle='-')
         colors = ['black', 'white']
         cmap = ListedColormap(colors)
-        self.ani = FuncAnimation(self.fig, animate, fargs=(self, target_x, target_y), interval=self.interval)
+        self.ani = FuncAnimation(self.fig, animate, fargs=(self,), interval=self.interval)
         self.grid_img = self.ax.imshow(self.map, origin='lower', cmap=cmap)
 
-        # add the initial position of the rover as a red dot
+        # Add the initial position of the rover as a red dot
         arrow_length = 1
         arrow_width = 2
         self.rover_arrow = Arrow(self.rover_x - arrow_length / 2, self.rover_y - arrow_width / 2, arrow_length, arrow_width, color='gray', zorder=2)
         self.ax.add_patch(self.rover_arrow)
 
+        # Add the destination positions as blue dots and number them
+        self.target_dots = []
+        for i, (target_x, target_y) in enumerate(self.targets):
+            target_dot = self.ax.scatter(target_x, target_y, c='blue')
+            self.ax.text(target_x + 0.5, target_y + 0.5, str(i + 1), fontsize=12, color='blue')
+            self.target_dots.append(target_dot)
 
-
-        # add the destination position as a blue dot
-        self.target_dot = self.ax.scatter(target_x, target_y, c='blue')
 
 
     def update_visualization(self, target_x, target_y):
@@ -86,20 +91,15 @@ class GridMapSimulator:
             # plt.close(self.fig)  # Stop the animation
             # exit(1)
 
-        return [self.grid_img, self.rover_arrow, self.target_dot, self.path_plot]
-
-
-
+        return [self.grid_img, self.rover_arrow, *self.target_dots, self.path_plot]
 
 
     def find_path(self, start_x, start_y, goal_x, goal_y):
-        # Define the A* heuristic function (diagonal distance)
         def heuristic(a, b):
             dx = abs(b[0] - a[0])
             dy = abs(b[1] - a[1])
             return (dx + dy) + (np.sqrt(2) - 2) * min(dx, dy)
 
-        # Initialize the A* algorithm
         frontier = PriorityQueue()
         frontier.put((0, (start_x, start_y)))
         came_from = {}
@@ -107,12 +107,10 @@ class GridMapSimulator:
         came_from[(start_x, start_y)] = None
         cost_so_far[(start_x, start_y)] = 0
 
-        # Run the A* algorithm
         while not frontier.empty():
             current = frontier.get()[1]
 
             if current == (goal_x, goal_y):
-                # If the goal has been reached, reconstruct the path and return it
                 path = [current]
                 while current != (start_x, start_y):
                     current = came_from[current]
@@ -122,12 +120,15 @@ class GridMapSimulator:
             for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]:
                 neighbor = (current[0] + dx, current[1] + dy)
 
-                # Check for diagonal collisions
+                if neighbor[0] < 0 or neighbor[0] >= self.map_width or neighbor[1] < 0 or neighbor[1] >= self.map_height:
+                    continue
+
                 if (dx != 0 and dy != 0) and (self.map[current[1], current[0] + dx] == -1 or self.map[current[1] + dy, current[0]] == -1):
                     continue
 
-                if neighbor[0] < 0 or neighbor[0] >= self.map_width or neighbor[1] < 0 or neighbor[1] >= self.map_height or self.map[neighbor[1], neighbor[0]] == -1:
+                if self.map[neighbor[1], neighbor[0]] == -1:
                     continue
+
                 new_cost = cost_so_far[current] + 1
                 if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
                     cost_so_far[neighbor] = new_cost
@@ -135,7 +136,6 @@ class GridMapSimulator:
                     frontier.put((priority, neighbor))
                     came_from[neighbor] = current
 
-        # If the goal could not be reached, return None
         return None
 
 
@@ -164,15 +164,15 @@ class GridMapSimulator:
             return None
 
 
-
-
-    def move_rover(self, target_x, target_y):
+    def move_rover(self):
         if self.reached_destination:
             return
         # Detect obstacles before moving
         detected_obstacle = self.detect_obstacle()
         if detected_obstacle:
             self.obstacles.append(detected_obstacle)
+
+        target_x, target_y = self.targets[self.current_target_index]
 
         # Find the optimal path from the current position to the target position using A*
         path = self.find_path(self.rover_x, self.rover_y, target_x, target_y)
@@ -203,25 +203,44 @@ class GridMapSimulator:
         # Check if the rover has reached the target position
         if self.rover_x == target_x and self.rover_y == target_y:
             print("I have made it to the destination!")
-            self.reached_destination = True  # Set the reached_destination attribute to True
-
+            self.reached_destination = True
+            if self.current_target_index + 1 < len(self.targets):
+                self.current_target_index += 1
+                self.reached_destination = False
+                print(f"Moving to the next target: {self.targets[self.current_target_index]}")
+            else:
+                print("All targets reached!")
 
 
 # Example usage
-
-def animate(frame, grid_map, target_x, target_y):
+def animate(frame, grid_map, *args):
     grid_map.detect_obstacle()
-    grid_map.move_rover(target_x, target_y)
+    grid_map.move_rover()
+    target_x, target_y = grid_map.targets[grid_map.current_target_index]
     return grid_map.update_visualization(target_x, target_y)
+
+def generate_random_targets(num_targets, map_width, map_height):
+    random_targets = []
+    for _ in range(num_targets):
+        x = random.randint(0, map_width - 1)
+        y = random.randint(0, map_height - 1)
+        random_targets.append((x, y))
+    return random_targets
+
 
 
 resolution = 1
-map_width = 35
-map_height = 35
+map_width = 50
+map_height = 50
 target_x, target_y = 30, 5
-initial_obstacles = 20
-animation_speed = 250
+initial_obstacles = 1
+animation_speed = 100
+num_targets = 3
 
-grid_map = GridMapSimulator(resolution, map_width, map_height, target_x, target_y, num_initial_obstacles=initial_obstacles, interval=animation_speed)
-grid_map.init_visualization(target_x, target_y)
+random_targets = generate_random_targets(num_targets, map_width, map_height)
+print("Random targets:", random_targets)
+
+grid_map = GridMapSimulator(resolution, map_width, map_height, random_targets, num_initial_obstacles=initial_obstacles, interval=animation_speed)
+target_x, target_y = random_targets[grid_map.current_target_index]
+grid_map.init_visualization()
 plt.show()
