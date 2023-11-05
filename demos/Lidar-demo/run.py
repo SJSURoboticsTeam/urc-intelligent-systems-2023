e42be0632bb26208416265e1e47dae11ba845720
Rahul Kandekar <rahukand@gmail.com>
2023-11-04 17:40:30 -0700

    Speed up the lidar rendering on matplotlib

--- demos/Lidar-demo/run.py
+++ demos/Lidar-demo/run.py
@@ -3,65 +3,76 @@ from rplidar import RPLidar
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 from matplotlib.axes import Axes
+from threading import Thread
+import time
 
 PORT_NAME = 'COM5'
-lidar = RPLidar(PORT_NAME)
-lidar_iter = iter(lidar.iter_scans(max_buf_meas=10000))
-
-
-def process_data(data):
-    # for angle in range(360):
-    #     distance = data[angle]
-    #     print(distance)
-    print()
-    print(data)
-    print()
-    data = [x for x in data if x != 0]
-    min_distance = min(data)
-    max_distance = max(data)
-    print("Minimum distance: ", min_distance)
-    print("Maximum distance: ", max_distance)
+lidar = None
+lidar_iter = None
+while True:
+    try:
+        lidar = RPLidar(PORT_NAME)
+        lidar_iter = iter(lidar.iter_scans(max_buf_meas=10000))
+        next(lidar_iter)
+    except Exception as e:
+        lidar.disconnect()
+        print(e)
+        continue
+    break
 
 scan_data = [0]*360
+
+def look(verbose=True):
+    global scan_data
+    scan = list(reversed(next(lidar_iter)))
+    for (_, angle, distance) in scan:
+        scan_data[floor(angle)%360] = distance
+    print()
+    print(scan_data)
+    print()
+def spin(stop_check, verbose=True):
+    while not stop_check():
+        look()
+
+stop_spinning=False
+t = Thread(target=spin, args=(lambda: stop_spinning, ))
+t.start()
 
 fig, axs = plt.subplots(ncols=2, subplot_kw={'projection': 'polar'})
 axl: Axes = axs[0]
 axl.set_title('Upside_down_lidar')
+axl.set_rmax(4000)
+scatL = axl.scatter([], [], marker='.')
+
 axr: Axes = axs[1]
 axr.set_title('Upside_up_lidar')
+axr.set_rmax(4000)
+scatR = axr.scatter([], [], marker='.')
 def update(_):
-    global lidar, lidar_iter
-    try:
-        scan = next(lidar_iter) 
-    except Exception as e:
-        lidar.disconnect()
-        lidar = RPLidar(PORT_NAME)
-        lidar_iter = iter(lidar.iter_scans(max_buf_meas=10000))
-        print(e)
-        return
-    scan = list(reversed(scan))
-    for (_, angle, distance) in scan:
-        scan_data[min([359, floor(angle)])] = distance
-    # process_data(scan_data)
-    axl.clear()
-    axl.plot([2*pi/360*i for i in range(0,360)], scan_data, ".")
     scan_data_rev = list(reversed(scan_data))
-    axl.set_title('Upside_down_lidar')
-    axr.clear()
-    axr.plot([2*pi/360*i for i in range(0,360)], scan_data_rev, ".")
-    axr.set_title('Upside_up_lidar')
-    fig.tight_layout()
+    # RMAX=max(scan_data_rev)*1.2
+    # axr.set_rmax(RMAX)
+    scatR.set_offsets([(2*pi/360*i, scan_data_rev[i]) for i in range(0,360)])
 
-    return
+    # axl.set_rmax(RMAX)
+    scatL.set_offsets([(2*pi/360*i, scan_data[i]) for i in range(0,360)])
+    return [scatR, scatL]
 
 anime = anim.FuncAnimation(
     fig,
     update,
     1,
     interval=1,
-    # blit=True
+    blit=True
 )
 plt.show()
+
+
+
+
+
+stop_spinning=True
+t.join()
 lidar.stop()
 lidar.stop_motor()
 lidar.disconnect()
