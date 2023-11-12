c7e17dad268de91bedfbf5fc084c8bd59aab05f1
Rahul <rahukand@gmail.com>
2023-11-11 19:03:23 -0800

    Implemented 2 features
    - neighboring points on lidar scans are joined if they are near each other
    - lidar distances are auto scaled from 0-1 so they always fit in the visualization field

--- demos/Lidar-demo/run.py
+++ demos/Lidar-demo/run.py
@@ -3,10 +3,11 @@ from rplidar import RPLidar
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 from matplotlib.axes import Axes
+from matplotlib.collections import LineCollection
 from threading import Thread
 import time
 
-PORT_NAME = 'COM5'
+PORT_NAME = 'COM6'
 lidar = None
 lidar_iter = None
 while True:
@@ -27,9 +28,9 @@ def look(verbose=True):
     scan = list(reversed(next(lidar_iter)))
     for (_, angle, distance) in scan:
         scan_data[floor(angle)%360] = distance
-    print()
-    print(scan_data)
-    print()
+    # print()
+    # print(scan_data)
+    # print()
 def spin(stop_check, verbose=True):
     while not stop_check():
         look()
@@ -38,34 +39,73 @@ stop_spinning=False
 t = Thread(target=spin, args=(lambda: stop_spinning, ))
 t.start()
 
-fig, axs = plt.subplots(ncols=2, subplot_kw={'projection': 'polar'})
-axl: Axes = axs[0]
-axl.set_title('Upside_down_lidar')
-axl.set_rmax(4000)
-scatL = axl.scatter([], [], marker='.')
+try:
+    fig, axs = plt.subplots(ncols=2, subplot_kw={'projection': 'polar'})
+    axl: Axes = axs[0]
+    # axl.axis('off')
+    axl.set_title('Upside_down_lidar')
+    axl.set_rmax(1)
+    # axl.set_rmin(0)
+    scatL = axl.scatter([], [], marker='o', c='orange')
+    obs_collectionL = LineCollection([])
+    axl.add_collection(obs_collectionL)
 
-axr: Axes = axs[1]
-axr.set_title('Upside_up_lidar')
-axr.set_rmax(4000)
-scatR = axr.scatter([], [], marker='.')
-def update(_):
-    scan_data_rev = list(reversed(scan_data))
-    # RMAX=max(scan_data_rev)*1.2
-    # axr.set_rmax(RMAX)
-    scatR.set_offsets([(2*pi/360*i, scan_data_rev[i]) for i in range(0,360)])
+    axr: Axes = axs[1]
+    # axr.axis('off')
+    axr.set_title('Upside_up_lidar')
+    axr.set_rmax(1) # All distances will be scaled to a 0-1 range
+    # axr.set_rmin(0)
+    scatR = axr.scatter([], [], marker='o', c='orange')
+    obs_collectionR = LineCollection([])
+    axr.add_collection(obs_collectionR)
+    def join_near_points(points, flipped = False, thresh=0.2):
+        """Given points in polar coordinates,
+        Returns a 2D List of obstacles, 
+        where each obstacle is a collection of points near each other"""
+        obstacles = [[(0,points[0])]]
+        for i, p in enumerate(points[1:]):
+            if abs(p-obstacles[-1][-1][1]) > thresh: # If point p is thresh far away from the last point in the last obstacle, then it is part of a new obstacle
+                obstacles.append([])
+            angle  = 2*pi/360*(i+1) if not flipped else 2*pi/360*(360-i+1)
+            obstacles[-1].append((angle,p))
+        # check if first and last obstacles are actually the same obstacles
+        if (obstacles[-1][-1][1]-obstacles[0][0][1]):
+            obstacles[0] = obstacles[-1] + obstacles[0]
+            obstacles.pop()
+        print()
+        print(obstacles)
+        print()
+        return obstacles
+    def update(_):
+        modded = []
+        # scan_data_rev = list(reversed(scan_data))
+        # RMAX=max(scan_data)*1.2
+        # axr.set_rmax(RMAX)
+        # scatR.set_offsets([(2*pi/360*(359-i), scan_data[i]) for i in range(0,360)])
+        # modded.append(scatR)
+        maxR = max(scan_data)*1.1
+        scaled_data = [i/maxR for i in scan_data]
+        obs_collectionR.set_segments(join_near_points(scaled_data, True))
+        modded.append(obs_collectionR)
 
-    # axl.set_rmax(RMAX)
-    scatL.set_offsets([(2*pi/360*i, scan_data[i]) for i in range(0,360)])
-    return [scatR, scatL]
+        # axl.set_rmax(RMAX)
+        # scatL.set_offsets([(2*pi/360*i, scan_data[i]) for i in range(0,360)])
+        # modded.append(scatL)
+        obs_collectionL.set_segments(join_near_points(scaled_data))
+        modded.append(obs_collectionL)
 
-anime = anim.FuncAnimation(
-    fig,
-    update,
-    1,
-    interval=1,
-    blit=True
-)
-plt.show()
+        return modded
+
+    anime = anim.FuncAnimation(
+        fig,
+        update,
+        1,
+        interval=1,
+        blit=True
+    )
+    plt.show()
+except Exception as e:
+    print(e)
 
 
 
