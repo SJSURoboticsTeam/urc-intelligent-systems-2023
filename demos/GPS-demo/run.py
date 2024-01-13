677b9960988fc4cada1760cdb5939013b5daf14d
Maaz0070 <67044481+Maaz0070@users.noreply.github.com>
2024-01-13 12:09:09 -0800

    navigation working

--- demos/GPS-demo/run.py
+++ demos/GPS-demo/run.py
@@ -7,7 +7,7 @@ from modules.GPS import gpsRead
 
 if __name__ == '__main__':
     try:
-        data = gpsRead("/dev/ttyACM0", 57600)
+        data = gpsRead("/dev/tty.usbmodem14101", 57600)
         print("GPS Port found")
         url = "http://192.168.1.133:5000/gps"
     except:
