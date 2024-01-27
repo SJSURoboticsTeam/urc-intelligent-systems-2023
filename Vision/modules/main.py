627951ffa201b5e121b21af30aa8324ad133b6c2
Gael <gaelgilbello27@gmail.com>
2024-01-27 13:23:16 -0800

    added mallet-water detection model for testing

---
+++ Vision/modules/main.py
@@ -0,0 +1,14 @@
+from depthai_sdk import OakCamera, ArgsParser
+import argparse
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-conf", "--config", help="Trained YOLO json config path", default='trained_models/mallet_water/best.json', type=str)
+args = ArgsParser.parseArgs(parser)
+
+with OakCamera(args=args) as oak:
+    color = oak.create_camera('color')
+    nn = oak.create_nn(args['config'], color, nn_type='yolo', spatial=True)
+    oak.visualize(nn, fps=True, scale=2/3)
+    oak.visualize(nn.out.passthrough, fps=True)
+    oak.start(blocking=True)
---
+++ Vision/modules/trained_models/mallet_water/README.md
@@ -0,0 +1,4 @@
+Yolo-v8 model trained on a custom mallet and plastic water bottle dataset. 
+Includes best.pt. The input size is 608x608. The model was trained for 50 epochs on 
+training set of size 1600.  
+These are the test results. 
