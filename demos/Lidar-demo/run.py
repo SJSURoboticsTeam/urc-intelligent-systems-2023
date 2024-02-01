35d0a63aece0bf89fab2f682203cb730ba36323d
Rahul <rahukand@gmail.com>
2024-01-31 17:27:46 -0800

    Seperated different responsibalities into different threads

--- demos/Lidar-demo/run.py
+++ demos/Lidar-demo/run.py
@@ -1,3 +1,24 @@
+"""
+Approach to running the Lidar set up.
+I'm thinking of running a separate thread for each and every task being run for the Lidar navigation.
+Here are the tasks that will each get its own thread.
+    1. Lidar Data Consumption:
+        The Lidar produces a lot of sensor data. This data needs to be consumed quickly 
+        or else it will build up inside the Lidar's internal buffer. This build up creates
+        a lag in the delivery of Lidar data. To prevent this there will be a dedicated thread
+        to consume data from the Lidar and make it available to other threads.
+    2. Visualization:
+        I'm a visual person and visualization helps to understand whats going on in code. So I'm
+        making this thread to visualize all code activity. This thread will visualize what the 
+        Lidar sees, it will visualize the navigation planning and anything else that will be helpful
+        to visualize. Important! - This should not be where processing should happen. When visualization 
+        is not needed it should be possible to just stop this thread and everything else should run normally
+    3.  Data maintenance:
+        A separate thread is to process Lidar data into point cloud with persistance and to convert that
+        into obstacles
+
+"""
+
 from math import floor, pi, sqrt, cos
 from rplidar import RPLidar
 import matplotlib.pyplot as plt
@@ -30,11 +51,13 @@ while True:
 #====================================
 # Running the Lidar
 #------------------------------------
-scan_data = list() # buffer to hold distance data
+scan_data = list() # buffer to hold distance data # use same ters scan/measures/
 def fill_measures(distances):
+    "Sets and array of (signal quality, angle (rad), distance (m))"
     global scan_data
     scan_data = distances
 def get_measures(flipped=True):
+    "Gets and array of (signal quality, angle (rad), distance (m))"
     sign = -1 if flipped else 1
     distances = [(quality, sign*2*pi*angle/360, distance/1000.0) for quality, angle, distance in scan_data]
     return distances
@@ -44,10 +67,12 @@ def look():
     fill_measures(next(lidar_iter))
 def spin(stop_check):
     """Keep fetching and updating buffer with distance data"""
-    while not stop_check():
-        look()
+    while not stop_check(): # You will notice there is no sleep to rest between loops.
+        look()              # Thats cuz next(lidar_iter) is blocking for a while.
+                            # Also cuz we are literally trying to consume as fast as possible 
+                            # to prevent data build up in buffer
 stop_spinning=False
-lidar_thread = Thread(target=spin, args=(lambda: stop_spinning, ))
+lidar_thread = Thread(target=spin, args=(lambda: stop_spinning, ), name="Lidar_consumption_thread")
 lidar_thread.start()
 #====================================
 
@@ -60,25 +85,49 @@ def update_measurement():
     global measurement_history
     measurement_history = [get_measures(),*measurement_history[:-1]]
 
-#==========================================
+#-------------------------------------------
 # Joining Neighboring points into obstacles
-#------------------------------------------
-thresh = 0.01
+# Maintain Obstacles in a thread (maybe I've created too many threads)
+
+thresh = 1 # In meters
+obstacles = None    # List of obstacles
+                    # Each obstacle is a list of points
+                    # None if not initialized
 def point_distance(p1, p2):
-    d = sqrt(p1[0]**2 + p2[0]**2 + 2*p1[0]*p2[0]*cos(p1[1]-p2[1]))
-    print(d)
-    return d
+    d1 = sqrt(abs(p1[1]**2 + p2[1]**2 - 2*p1[1]*p2[1]*cos(p1[0]-p2[0])))
+    return d1
 def join_points(points):
-    return []
-    groups = [[points[0]]]
-    for p in points[1:]:
-        if point_distance(groups[-1][-1], p) < thresh: # last group's last point
+    piter = iter(points)
+    try:
+        groups = [[next(piter)]]
+    except StopIteration:
+        return None
+    for p in piter:
+        if point_distance(groups[-1][-1], p) > thresh: # last group's last point
             groups.append([])
         groups[-1].append(p)
-    if point_distance(groups[0][0], groups[-1][-1]) > thresh and len(groups) > 1:
+    if point_distance(groups[0][0], groups[-1][-1]) < thresh and len(groups) > 1:
         groups[0] = groups[0] + groups[-1]
         groups.pop()
     return groups
+
+#----------------------------------------------------------
+# Start up the thread to maintain point cloud and obstacles
+
+def keep_updating_obstacles(stop_updating):
+    global obstacles
+    while not stop_updating():
+        time.sleep(0.05)
+        update_measurement()
+        measures = sum(measurement_history, [])
+        measures = sorted(measures, key=lambda i: i[1])
+        obstacles = join_points(((a,d) for q,a,d in measures)) 
+stop_updating_obstacles = False
+obstacle_thread = Thread(target=keep_updating_obstacles, args=(lambda: stop_updating_obstacles, ), name="Obstacle_maintaining_thread")
+obstacle_thread.start()
+
+
+
 
 
 
@@ -86,34 +135,27 @@ def join_points(points):
 try:
     fig = plt.figure()
     ax = plt.subplot(111, projection='polar')
-    obstacle_points = ax.scatter([],[], s=5, c=[], cmap=plt.cm.Greys)
-    obstacle_groups = LineCollection([])
+    obstacle_points = ax.scatter([],[], s=1)
+    obstacle_groups = LineCollection([], color=(0,0,0,0.5), linewidths=5)
     ax.add_collection(obstacle_groups)
-
-
-
 
     rmax=5
     rmax_=0.01
     ax.set_rmax(rmax)
     def update_plot(_):
         modded = []
-        update_measurement()
         # Visualizing Point Cloud
-        mes_his = list(reversed(measurement_history))
-        measures = sum(mes_his, [])
-        measurement_freshness = [ [i]*len(m) for i, m in enumerate(mes_his)]
-        measurement_freshness = sum(measurement_freshness, [])
-        obstacle_points.set_offsets([(m[1], m[2]) for m in measures])
-        obstacle_points.set_array(measurement_freshness)
-        modded.append(obstacle_points)
+        mes_his = list(reversed(measurement_history))                           # If someone
+        measures = sum(mes_his, [])                                             # could just
+        measurement_freshness = [ [i]*len(m) for i, m in enumerate(mes_his)]    # reduced the lines
+        measurement_freshness = sum(measurement_freshness, [])                  # of code here
+        alphas = [i/history_size for i in measurement_freshness]                # I'd love it      
+        obstacle_points.set_offsets([(m[1], m[2]) for m in measures])           # 
+        obstacle_points.set_color(list(zip(*[[0]*len(alphas)]*3, alphas)))      # 
+        modded.append(obstacle_points)                                          #
         #---------------------
         # Visualizing Obstacles
-        # print(measurement_history)
-        segments = join_points([(ang,dist) for _,ang,dist in measurement_history[0]])
-        obstacle_groups.set_segments(segments)
-        # print(segments)
-        # print()
+        obstacle_groups.set_segments(obstacles)
         modded.append(obstacle_groups)
         #---------------------
         return modded
@@ -126,83 +168,8 @@ except Exception as e:
 
 
 
-
-
-
-# try:
-#     fig, axs = plt.subplots(ncols=2, subplot_kw={'projection': 'polar'})
-#     axl: Axes = axs[0]
-#     # axl.axis('off')
-#     axl.set_title('Upside_down_lidar')
-#     axl.set_rmax(1)
-#     # axl.set_rmin(0)
-#     scatL = axl.scatter([], [], marker='o', c='orange')
-#     obs_collectionL = LineCollection([])
-#     axl.add_collection(obs_collectionL)
-#     axl.set_visible(False)
-
-#     axr: Axes = axs[1]
-#     # axr.axis('off')
-#     axr.set_title('Upside_up_lidar')
-#     axr.set_rmax(1) # All distances will be scaled to a 0-1 range
-#     # axr.set_rmin(0)
-#     scatR = axr.scatter([], [], marker='o', c='orange')
-#     obs_collectionR = LineCollection([])
-#     axr.add_collection(obs_collectionR)
-#     def join_near_points(points, flipped = False, thresh=0.2):
-#         """Given points in polar coordinates,
-#         Returns a 2D List of obstacles, 
-#         where each obstacle is a collection of points near each other"""
-#         obstacles = [[(0,points[0])]]
-#         for i, p in enumerate(points[1:]):
-#             if abs(p-obstacles[-1][-1][1]) > thresh: # If point p is thresh far away from the last point in the last obstacle, then it is part of a new obstacle
-#                 obstacles.append([])
-#             angle  = 2*pi/360*(i+1) if not flipped else 2*pi/360*(360-i+1)
-#             obstacles[-1].append((angle,p))
-#         # check if first and last obstacles are actually the same obstacles
-#         if (obstacles[-1][-1][1]-obstacles[0][0][1] <= thresh):
-#             obstacles[0] = obstacles[-1] + obstacles[0]
-#             obstacles.pop()
-#         print()
-#         print(obstacles)
-#         print()
-#         return obstacles
-#     def update(_):
-#         modded = []
-#         # scan_data_rev = list(reversed(scan_data))
-#         # RMAX=max(scan_data)*1.2
-#         # axr.set_rmax(RMAX)
-#         # scatR.set_offsets([(2*pi/360*(359-i), scan_data[i]) for i in range(0,360)])
-#         # modded.append(scatR)
-#         maxR = max(scan_data)*1.1
-#         scaled_data = [i/maxR for i in scan_data]
-#         obs_collectionR.set_segments(join_near_points(scaled_data, True))
-#         modded.append(obs_collectionR)
-
-#         # axl.set_rmax(RMAX)
-#         # scatL.set_offsets([(2*pi/360*i, scan_data[i]) for i in range(0,360)])
-#         # modded.append(scatL)
-
-#         # obs_collectionL.set_segments(join_near_points(scaled_data))
-#         # modded.append(obs_collectionL)
-
-#         return modded
-
-#     anime = anim.FuncAnimation(
-#         fig,
-#         update,
-#         1,
-#         interval=1,
-#         blit=True
-#     )
-#     plt.show()
-# except Exception as e:
-#     print(e)
-
-
-
-
-
+stop_updating_obstacles=True
+obstacle_thread.join()
 stop_spinning=True
 lidar_thread.join()
 lidar.stop()
