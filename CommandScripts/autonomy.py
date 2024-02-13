bd69cb208000ee003d7bbe7bb9619e5bc2fdb73f
chrehall68 <chrehall68@gmail.com>
2024-02-13 15:17:48 -0800

    add compass calibrations, stopping@beacons, LS

--- CommandScripts/autonomy.py
+++ CommandScripts/autonomy.py
@@ -3,6 +3,7 @@ import json
 import requests
 from CommandScripts import Trajectory
 import os, sys
+
 sys.path.insert(0, os.path.abspath(".."))
 from CommandScripts.GPS_NAV import GPS_Nav
 from CommandScripts import AutoHelp
@@ -11,14 +12,19 @@ from modules.WiFi import WiFi
 import time
 import threading
 
+
 class Autonomy:
-    def __init__(self, rover_comms, url, max_speed, max_steering, GPS, GPS_coordinate_map):
+    def __init__(
+        self, rover_comms, url, max_speed, max_steering, GPS, GPS_coordinate_map
+    ):
         self.rover_comms = rover_comms
         self.url = url
         self.compass = Compass()
         self.GPS = GPS
 
-        self.GPS_Nav = GPS_Nav(max_speed, max_steering, self.GPS, self.compass, GPS_coordinate_map)
+        self.GPS_Nav = GPS_Nav(
+            max_speed, max_steering, self.GPS, self.compass, GPS_coordinate_map
+        )
         self.AutoHelp = AutoHelp.AutoHelp()
 
         self.current_GPS = None
@@ -31,10 +37,14 @@ class Autonomy:
                 if new_GPS is not None:
                     self.current_GPS = new_GPS
 
-
     def get_rover_status(self, bearing, distance):
         try:
-            json_command = {"Bearing":bearing,"Distance":distance,"GPS":[self.current_GPS[0],self.current_GPS[1]],"Target":[self.GPS_Nav.GPS_target[0],self.GPS_Nav.GPS_target[1]]}
+            json_command = {
+                "Bearing": bearing,
+                "Distance": distance,
+                "GPS": [self.current_GPS[0], self.current_GPS[1]],
+                "Target": [self.GPS_Nav.GPS_target[0], self.GPS_Nav.GPS_target[1]],
+            }
             # json_command = json.dumps(json_command)
             # json_command = json_command.replace(" ", "")
             requests.post(f"{self.url}/autonomy", json=json_command)
@@ -46,16 +56,36 @@ class Autonomy:
         gps_thread.start()
 
         homing_end = "Starting control loop..."
-        self.rover_comms = WiFi("http://192.168.0.101:5001")
+        self.rover_comms = WiFi("http://192.168.0.211:5000")
         response = self.rover_comms.read_data()
+
+        # blue first
+        self.rover_comms.write_data(
+            {"HB": 0, "IO": 1, "WO": 0, "DM": "D", "CMD": [0, 0], "LS": 2}
+        )
+        time.sleep(5)
+
+        # send red
+        self.rover_comms.write_data(
+            {"HB": 0, "IO": 1, "WO": 0, "DM": "D", "CMD": [0, 0], "LS": 1}
+        )
+        time.sleep(1)
 
         while True:
             with self.GPS_lock:
                 current_GPS = self.current_GPS
             if current_GPS and current_GPS != "Need More Satellite Locks":
-                command = self.GPS_Nav.get_steering(current_GPS, self.GPS_Nav.GPS_target)
-                bearing = round(self.AutoHelp.get_bearing(current_GPS, self.GPS_Nav.GPS_target), 3)
-                distance = round(self.AutoHelp.get_distance(current_GPS, self.GPS_Nav.GPS_target)[0]*1000, 3)
+                command = self.GPS_Nav.get_steering(
+                    current_GPS, self.GPS_Nav.GPS_target
+                )
+                bearing = round(
+                    self.AutoHelp.get_bearing(current_GPS, self.GPS_Nav.GPS_target), 3
+                )
+                distance = round(
+                    self.AutoHelp.get_distance(current_GPS, self.GPS_Nav.GPS_target)[0]
+                    * 1000,
+                    3,
+                )
                 print("Current GPS:", current_GPS)
                 print("Target GPS:", self.GPS_Nav.GPS_target)
                 print("Heading:", self.compass.get_heading())
@@ -65,7 +95,19 @@ class Autonomy:
                 response = self.rover_comms.read_data()
                 self.get_rover_status(bearing, distance)
                 if response != "No data received" and command != None:
+                    reachedDestination = False
+                    if "LS" not in command:
+                        command["LS"] = 1  # red = autonomously driving = 1
+                    else:
+                        reachedDestination = True
+
+                    command["IO"] = 1  # force IO
                     self.rover_comms.write_data(command)
+
+                    # if we've been ordered to stop (reached our destination, wait)
+                    if reachedDestination:
+                        time.sleep(5)
+                time.sleep(0.1)
             else:
                 print("GPS Error. Current GPS:", current_GPS)
                 time.sleep(1)
--- modules/LSM303.py
+++ modules/LSM303.py
@@ -3,15 +3,62 @@ import board
 import adafruit_lsm303dlh_mag
 import math
 
+
 class Compass:
+    # calibration results from calibrate.py
+    hard_iron_bias_x = 18.87701934
+    hard_iron_bias_y = 58.4384496
+    hard_iron_bias_z = 26.31685794
+
+    soft_iron_bias_xx = 22.266777324823483
+    soft_iron_bias_xy = -0.028101845326386522
+    soft_iron_bias_xz = 0.5375605726239839
+
+    soft_iron_bias_yx = -0.02810184532638666
+    soft_iron_bias_yy = 22.223229896289904
+    soft_iron_bias_yz = 0.8679722456023597
+
+    soft_iron_bias_zx = 0.5375605726239857
+    soft_iron_bias_zy = 0.8679722456023594
+    soft_iron_bias_zz = 21.339926725974067
+
     def __init__(self):
         self.i2c = board.I2C()
+
         self.mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(self.i2c)
+        self.mag.mag_rate = adafruit_lsm303dlh_mag.MAGRATE_75
 
     def get_heading(self):
-        x = round(self.mag.magnetic[0], 3)
-        y = round(self.mag.magnetic[1], 3)
-        heading = math.atan2(y,x)*(180/math.pi)
+        # x, y, z = self.mag.magnetic
+        x_off = self.mag.magnetic[0] - self.hard_iron_bias_x
+        y_off = self.mag.magnetic[1] - self.hard_iron_bias_y
+        z_off = self.mag.magnetic[2] - self.hard_iron_bias_z
+        x = (
+            x_off * self.soft_iron_bias_xx
+            + y_off * self.soft_iron_bias_yx
+            + z_off * self.soft_iron_bias_zx
+        )
+        y = (
+            x_off * self.soft_iron_bias_xy
+            + y_off * self.soft_iron_bias_yy
+            + z_off * self.soft_iron_bias_zy
+        )
+        # z = (
+        #     x_off * self.soft_iron_bias_xz
+        #     + y_off * self.soft_iron_bias_yz
+        #     + z_off * self.soft_iron_bias_zz
+        # )
+
+        heading = math.atan2(y, x) * (180 / math.pi)
+
+        # use y as the anchor, not x
+        # so that we get 0* as North
+        # this is done by just subtracting 90*
+        heading -= 90
+
         if heading < 0:
             heading += 360
         return round(heading, 3)
+
+    def get_raw(self):
+        return self.mag.magnetic
