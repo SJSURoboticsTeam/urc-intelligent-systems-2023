f4602c00a2d9b9cfd1a3ed2cfed91305eaf6bf58
Rahul <rahukand@gmail.com>
2024-03-01 13:49:56 -0800

    Implemented a clean disconnect code in the client code

--- unified_frameworks/sensor_array/websocket_client.py
+++ unified_frameworks/sensor_array/websocket_client.py
@@ -4,8 +4,10 @@ import websockets
 async def receive_data():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        while True:
+        count = 0
+        while count < 10:
             data = await websocket.recv()
             print(f"Received: {data}")
+            count+=1
 
 asyncio.get_event_loop().run_until_complete(receive_data())
