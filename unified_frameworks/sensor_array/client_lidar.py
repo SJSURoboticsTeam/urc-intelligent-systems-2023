e9f20dd7a9f4bde4b1791f4ffcc35127b046f474
Rahul <rahukand@gmail.com>
2024-03-01 16:06:10 -0800

    Tested and confirmed fast streaming of lidar data

--- unified_frameworks/sensor_array/client_lidar.py
+++ unified_frameworks/sensor_array/client_lidar.py
@@ -2,6 +2,7 @@ import asyncio
 import websockets
 from LidarClass import Lidar
 from threading import Thread
+import json
 
 class WirelessLidar(Lidar):
     def __init__(self, uri):
@@ -13,7 +14,8 @@ class WirelessLidar(Lidar):
     async def receive_data(self):
         async with websockets.connect(self.uri) as websocket:
             while self.connected:
-                self.data = await websocket.recv()
+                data = await websocket.recv()
+                self.data = json.loads(data)
     def start_connection(self):
         asyncio.set_event_loop(asyncio.new_event_loop())
         asyncio.get_event_loop().run_until_complete(self.receive_data())
@@ -33,14 +35,15 @@ class WirelessLidar(Lidar):
 
 if __name__=='__main__':
     import time
-    lidar = WirelessLidar("ws://192.168.0.101:8765")
+    lidar = WirelessLidar("ws://192.168.1.107:8765")
     # lidar = WirelessLidar("ws://localhost:8765")
     lidar_iter = iter(lidar)
     start_time = time.time()
     count = 0
     while time.time() - start_time < 5:
         count += 1
+        print()
         print(f"{count}: {next(lidar_iter)}")
-        time.sleep(0.4)
+        # time.sleep(0.005)
     lidar.disconnect()
     print("disconnected")
